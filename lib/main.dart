057c05216ca8a9406f2e3efa55795742e3d6c202
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:19:30 +0800

    Main: replace main with HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:rick_morty_app/home/repositories/rick_morty_repository.dart';
+import 'package:provider/provider.dart';
+import 'package:rick_morty_app/home/provider/home_provider.dart';
+import 'package:rick_morty_app/home/widgets/page/home_page.dart';
 import 'package:rick_morty_app/util/get_it_util.dart';
 
 void main() {
@@ -19,60 +21,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    final result = await getIt<RickMortyRepository>().fetchCharacters();
-
-    result.fold(
-      (error) {},
-      (response) {},
-    );
-
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: ChangeNotifierProvider(
+        create: (_) => HomeProvider(),
+        child: const HomePage(),
       ),
     );
   }
