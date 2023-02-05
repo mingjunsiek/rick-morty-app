03392795d2b1751d9671857da1ba97619f0b5c1f
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:17:24 +0800

    Provider: set up HomeProvider

---
+++ lib/home/provider/home_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:rick_morty_app/common/models/character.dart';
+import 'package:rick_morty_app/home/repositories/rick_morty_repository.dart';
+import 'package:rick_morty_app/util/get_it_util.dart';
+import 'package:rick_morty_app/util/page_state.dart';
+
+class HomeProvider with ChangeNotifier {
+  List<Character> characters = [];
+  PageState pageState = PageState.isLoading;
+  final RickMortyRepository repository = getIt<RickMortyRepository>();
+
+  void onRetryTapped() {
+    setIsLoading();
+    fetchCharacters();
+  }
+
+  void setIsLoading() {
+    pageState = PageState.isLoading;
+    notifyListeners();
+  }
+
+  void fetchCharacters() async {
+    final result = await repository.fetchCharacters();
+    await Future.delayed(const Duration(seconds: 3));
+    result.fold(
+      (error) {
+        pageState = PageState.hasError;
+        notifyListeners();
+      },
+      (response) {
+        pageState = PageState.success;
+        characters = response;
+        notifyListeners();
+      },
+    );
+  }
+}
---
+++ lib/util/page_state.dart
@@ -0,0 +1,5 @@
+enum PageState {
+  isLoading,
+  hasError,
+  success,
+}
