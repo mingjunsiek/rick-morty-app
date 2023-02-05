2ffc9fd3cbf8050a45a76d5b931b2d8255ec4fb1
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:57:39 +0800

    Episode: set up episode section

---
+++ lib/character_detail/widgets/molecule/episode_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:rick_morty_app/common/models/character.dart';
+
+class EpisodeSection extends StatelessWidget {
+  const EpisodeSection({
+    super.key,
+    required this.character,
+  });
+  final Character character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Episodes',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        ...character.episode
+            .map((episode) => Text(
+                  episode,
+                ))
+            .toList(),
+      ],
+    );
+  }
+}
