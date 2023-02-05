ea6f943531066f00ee902a4fd1da493690a6d12c
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:57:55 +0800

    DetailPage: Connect character detail page

--- lib/character_detail/widgets/page/character_detail_page.dart
+++ lib/character_detail/widgets/page/character_detail_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rick_morty_app/character_detail/widgets/atom/character_image.dart';
+import 'package:rick_morty_app/character_detail/widgets/molecule/basic_information_section.dart';
+import 'package:rick_morty_app/character_detail/widgets/molecule/episode_section.dart';
+import 'package:rick_morty_app/character_detail/widgets/molecule/location_section.dart';
 import 'package:rick_morty_app/common/models/character.dart';
+import 'package:rick_morty_app/util/spacing.dart';
 
 class CharacterDetailPage extends StatelessWidget {
   const CharacterDetailPage({
@@ -13,6 +18,31 @@ class CharacterDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(character.name),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                CharacterImage(url: character.image),
+                Spacing.sp10,
+                BasicInformationSection(
+                  character: character,
+                ),
+                Spacing.sp10,
+                LocationSection(
+                  character: character,
+                ),
+                Spacing.sp10,
+                EpisodeSection(
+                  character: character,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
