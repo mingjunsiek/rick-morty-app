55f688e798b1a58ec76aac8e6e2a52605b7ce1de
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:56:46 +0800

    Spacing: add spacing util

---
+++ lib/util/spacing.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Spacing {
+  Spacing._();
+
+  static const Widget shrink = SizedBox.shrink();
+
+  static const SizedBox sp10 = SizedBox(
+    height: 10,
+    width: 10,
+  );
+}
