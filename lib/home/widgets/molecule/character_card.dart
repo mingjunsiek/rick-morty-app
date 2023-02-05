715e21714bae8a802764e3518069c664018eb780
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:39:01 +0800

    CharacterCard: set up route to detail page

--- lib/home/widgets/molecule/character_card.dart
+++ lib/home/widgets/molecule/character_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rick_morty_app/character_detail/widgets/page/character_detail_page.dart';
 import 'package:rick_morty_app/common/models/character.dart';
 
 class CharacterCard extends StatelessWidget {
@@ -10,21 +11,30 @@ class CharacterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Text(
-              'Name: ${character.name}',
-            ),
-            Text(
-              'Species: ${character.species}',
-            ),
-            Text(
-              'Status: ${character.status}',
-            ),
-          ],
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => CharacterDetailPage(
+            character: character,
+          ),
+        ),
+      ),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Text(
+                'Name: ${character.name}',
+              ),
+              Text(
+                'Species: ${character.species}',
+              ),
+              Text(
+                'Status: ${character.status}',
+              ),
+            ],
+          ),
         ),
       ),
     );
