77237b9b615fb57993670caf8f168926361ba3df
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 15:28:37 +0800

    HomePageBody: replace with CharacterCards

--- lib/home/widgets/organism/home_page_body.dart
+++ lib/home/widgets/organism/home_page_body.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rick_morty_app/home/provider/home_provider.dart';
+import 'package:rick_morty_app/home/widgets/molecule/character_card.dart';
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: context.read<HomeProvider>().characters.length,
-        itemBuilder: (_, index) {
-          final character = context.read<HomeProvider>().characters[index];
-          return Card(
-            child: Text(character.name),
-          );
-        });
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: ListView.builder(
+          itemCount: context.read<HomeProvider>().characters.length,
+          itemBuilder: (_, index) {
+            final character = context.read<HomeProvider>().characters[index];
+            return CharacterCard(
+              character: character,
+            );
+          }),
+    );
   }
 }
