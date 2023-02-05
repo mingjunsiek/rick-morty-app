37b4bbd54db663fde6bec321b51c615e25d3a35a
Ming Jun Siek <siekmingjun@hotmail.com>
2023-02-05 13:36:15 +0800

    Character: Update id to int

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:rick_morty_app/models/location.dart';
+
+import 'location.dart';
 
 part 'character.freezed.dart';
 part 'character.g.dart';
@@ -7,7 +8,7 @@ part 'character.g.dart';
 @freezed
 class Character with _$Character {
   const factory Character({
-    required String id,
+    required int id,
     required String name,
     required String status,
     required String species,
